af2ae8bf482c5e9169124401ffd603441fe74b93
martinD2014 <91673297+martinD2014@users.noreply.github.com>
2023-05-11 11:20:31 +0200

    Increase delay

--- frontend/static/js/script.js
+++ frontend/static/js/script.js
@@ -115,7 +115,7 @@ function setBotResponse(response) {
 	//display bot response after the number of miliseconds caputred by the variable 'delay_first_message'
 	var delay_first_message = 500;
 	if (response.length >=1) {
-		delay_first_message = Math.min(Math.max(response[0].text.length * 45, 800), 3000);
+		delay_first_message = Math.min(Math.max(response[0].text.length * 45, 800), 5000);
 		// delay_first_message = 20;
 	}
 	setTimeout(function () {
@@ -168,7 +168,7 @@ function setBotResponse(response) {
 		for (var i = 1; i < response.length; i++){
 			
 			//Add delay based on the length of the next message
-			summed_timeout += Math.min(Math.max(response[i].text.length * 45, 800), 3000);
+			summed_timeout += Math.min(Math.max(response[i].text.length * 45, 800), 5000);
 			doScaledTimeout(i, response, summed_timeout)
 			
 		}
